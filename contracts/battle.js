174e141499b51533dc58f44292c5b8574d370e0e
hloong <hbxglong@163.com>
2021-10-11 10:28:33 +0800

    Update battle.js
    zoon改版，去除合约交互功能

--- contracts/battle.js
+++ contracts/battle.js
@@ -1,254 +1,154 @@
+const config = require('./config');
+const ERC721 = require('./contracts/erc721');
 const Web3 = require('web3');
-const BigNumber = require('bignumber.js');
-const retry = require('bluebird-retry');
-const retryPromise = require('bluebird');
+const cache = require('./utils/cache');
+const { $get, $post } = require('./utils/axios');
+let yags = 0
+let battleCount = 0
+let winCount = 0
 
-const zoonAbi = require('./abi/Zoon.json');
-const config = require('../config');
-
-var i18n_module = require('i18n-nodejs');
-var i18n = new i18n_module(config.lang, './../../locale.json');
-
-function Battle(chain, contractAddress, privateKey) {
-
+function AutoBattle(config) {
     let self = this;
+    self.web3 = new Web3('https://bsc-dataseed.binance.org/');
+    self.config = config;
+    self.erc721Contract = new ERC721(config.chain, config.zoonNftContract);
+}
 
-    self.web3 = new Web3(chain.rpc);
-    self.chain = chain;
-    self.contractAddress = contractAddress;
+AutoBattle.prototype.run = async function () {
+    let self = this;
+    console.log(`ZOON自动Battle脚本启动，当前账号: ${self.config.address}`);
+    let timeOut = config.loopSeconds * 1000;
+    let count = 1;
+    (function iterator() {
+        self.runThis(count).then(() => {
+            setTimeout(() => {
+                count++;
+                iterator();
+            }, timeOut);
+        }).catch((err) => {
+            console.log(`error: ${err.message}`);
+            console.log(err);
+            setTimeout(() => {
+                count++;
+                iterator();
+            }, timeOut);
+        })
+    })();
+}
 
-    self.tokenContract = new self.web3.eth.Contract(zoonAbi, contractAddress);
-
-    if (!privateKey.startsWith('0x')) {
-        privateKey = '0x' + privateKey;
-    }
-    let account = self.web3.eth.accounts.privateKeyToAccount(privateKey);
+AutoBattle.prototype.runThis = async function (count) {
+    console.log(`============================程序开始第${count}次扫描`);
+    const self = this;
+    // 转码
+    const msg = self.web3.utils.utf8ToHex("zoanisthebestbscnft") 
+    // 生成钱包
+    const account = self.web3.eth.accounts.privateKeyToAccount(config.privateKey);
+    let token = ''
     self.web3.eth.accounts.wallet.add(account);
     self.web3.eth.defaultAccount = account.address;
-}
-
-Battle.prototype.battle = async function (itemId, index) {
-    let self = this;
-    let owner = self.web3.eth.defaultAccount;
-    let options = {
-        from: owner,
-        gas: self.chain.gasLimit,
-        gasPrice: self.chain.gasPrice,
-        value: 0
+    // 签名
+    self.web3.eth.sign(msg , account.address).then(function (res) {
+      token = res
+    })
+    const petsAvailableArr = []
+    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+    /// 获取宠物id列表
+    const petIds = await self.getPetIds(self.config.address);
+    const verifyWalletData = await $post('/verifyWallet',{
+          id: config.address,
+          signature:token
+    })
+    // 获得签名后的token
+    const verifyToken = verifyWalletData.user.token
+    const battleWalletData = await $post('/battle/wallet',{
+          verifyToken
+    })
+    // 挑出可战斗宠物
+    for (let i = 0; i < petIds.length; i++) {
+      const id = petIds[i];
+      const res = await $get(`/battle/tokenId/${id}`)
+      const totalTurn = res.data.totalTurn
+      console.log(`宠物id：${id}  当前可战斗：${totalTurn} 次`);
+      if(totalTurn>0){
+        petsAvailableArr.push({
+          id:res.message*1,
+          data:res.data
+        })
+      }
     }
-    console.log(i18n.__("Pets list", {itemId: itemId, index: index}));
-    let sendResult = await self.tokenContract.methods.battle(itemId, index).send(options);
-    return sendResult;
-}
+    console.log(`总宠物数量：${petIds.length}，可用宠物数量: ${petsAvailableArr.length}`);
+    if(petsAvailableArr.length === 0){
+      console.log('没有可战斗宠物')
+      return
+    }
 
-Battle.prototype.battleOfTransaction = function (itemId, index) {
-    let self = this;
-    let data = self.web3.eth.abi.encodeFunctionCall({
-        name: 'battle',
-        type: 'function',
-        inputs: [{
-            "name": "_id",
-            "type": "uint256"
-        }, {
-            "name": "_index",
-            "type": "uint8"
-        }]
-    }, [itemId.toString(), index.toString()]);
-
-    let owner = self.web3.eth.defaultAccount;
-    let transaction = {
-        from: owner,
-        to: self.contractAddress,
-        gas: self.chain.gasLimit,
-        gasPrice: self.chain.gasPrice,
-        value: 0,
-        data: data
-    };
-    return transaction;
-}
-
-Battle.prototype.getTransactionCount = async function () {
-    let self = this;
-    let owner = self.web3.eth.defaultAccount;
-    let transactionCount = await self.retryRequest(() => {
-        return new Promise((resolve, reject) => {
-            self.web3.eth.getTransactionCount(owner).then(function (result) {
-                resolve(result);
-            }).catch(function (error) {
-                reject(error);
-            });
-        });
-    });
-    return transactionCount;
-}
-
-
-/// 线性发送交易
-Battle.prototype.queueTransactions = async function (transactions) {
-    let self = this;
-    let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-    for(let i=0; i<transactions.length; i++) {
-        try {
-            let transaction = transactions[i];
-            let result = await self.sendTransactionAndRetry(transaction, i);
-            console.log(i18n.__("Transaction successfully", {current: i+1, trxID: result}));
-
-            if (config.attackResult) {
-
-                await wait(5000);
-  
-                for(let j=0; j<10; j++) {
-
-                    let logs = await retry(() => {
-                        return self.getTransactionReceipt(result);
-                    }, { max_tries: 5, interval: 3000 });
-
-                    if (logs != null) {
-
-                        try {
-
-                            let log = self.decodeLog(logs['logs'][2]['data']);
-
-                            let res = log['result'] == '0' ? 'Lose' : 'Win';
-
-                            let reward = self.web3.utils.fromWei(log['reward']);
-
-                            let exp = (parseInt(log['exp']) / 100).toFixed(2);
-
-                            console.log( i18n.__("Attack result", {result: i18n.__(res), reward: reward, exp: exp }) );
-
-                            break;
-
-                        } catch(err){
-                            console.log(err);
-                        }
-                    }
-
-                    await wait(2000);
-                }
-
-            }
-
-            await wait(3000);
-        } catch(err) {
-            console.log(i18n.__("Transaction failed", {current: i+1, err: err}));
+    for (let i = 0; i < petsAvailableArr.length; i++) {
+      const petItem = petsAvailableArr[i];
+      const count = petItem.data.totalTurn
+      const id = petItem.id
+      for (let j = 0; j < count; j++) {
+        // 延迟1.5秒，避免奖励未发放
+        await wait(1500)
+        const battle = await $post('/battle',{
+          token: verifyToken,
+          _monster: config.gameIndex,
+          _tokenId: id
+        })
+        const battleWalletData = await $post('/battle/wallet',{
+          token:verifyToken
+        })  
+        if(!battle.success){
+          console.log('没有result')
+          break;
         }
+        const result = battle.data.result
+        const exp = battle.data.exp/100
+        const reward = (battle.data.reward/1000000000000000000).toFixed(2)
+        const resultPetId = battle.data._tokenId
+        battleCount = battleCount + 1
+        if(result === 'win'){
+          yags = yags + (reward*1)
+          winCount = winCount + 1
+        }
+        console.log(
+        `本轮第${battleCount}次战斗  
+        id:${resultPetId}  
+        战斗结果：${result}  
+        战斗奖励：${reward}  
+        获得经验：${exp}  
+        本轮战斗共获得：${yags.toFixed(2)}   
+        当前胜率：${winCount/battleCount * 100}%`)
+      }
     }
+  console.log('本轮战斗结束')
+
 }
 
+/// 获取宠物id列表
+AutoBattle.prototype.getPetIds = async function (address) {
 
-
-Battle.prototype.retryRequest = async function (request) {
     let self = this;
-    return new Promise((resolve, reject) => {
-        /// 重试3次
-        retry(() => {
-            return request();
-        }, { max_tries: 5, interval: 3000 }).then(function (result) {
-            resolve(result);
-        }).catch(function (error) {
-            reject(error);
-        });
-    });
+    /// 获取当前账号有几个NFT
+    let balance = await self.erc721Contract.balanceOf(address);
+    let balanceCacheKey = 'pets_balance';
+    let balanceCache = cache.get(balanceCacheKey);
+    let balanceChanged = balanceCache != balance;
+    if (balanceChanged) {
+        cache.set(balanceCacheKey, balance, 1000000000);
+    }
 
+    let petIdsCacheKey = "pets_ids";
+    let results = cache.get(petIdsCacheKey);
+    if (!results || balanceChanged) {
+        let promises = [];
+        for (var i = 0; i < balance; i++) {
+            let p = self.erc721Contract.tokenOfOwnerByIndex(address, i);
+            promises.push(p);
+        }
+        results = await Promise.all(promises);
+        cache.set(petIdsCacheKey, results, 10 * 60);//用户余额不变的情况下，缓存10分钟的宠物id列表
+    }
+    return results;
 }
 
-Battle.prototype.sendTransactionAndRetry = async function (transaction, index) {
-    let self = this;
-    let i = 0;
-    return new Promise((resolve, reject) => {
-        /// 重试3次
-        retry(() => {
-            return self.sendTransaction(transaction, index);
-        }, { max_tries: 5, interval: 3000 }).then(function (result) {
-            resolve(result);
-        }).catch(function (error) {
-            reject(error);
-        });
-    });
-}
-
-Battle.prototype.sendTransaction = async function (transaction, index) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-        self.web3.eth.sendTransaction(transaction, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-}
-
-Battle.prototype.battleSessionsTime = async function (tokenId, rare) {
-    let self = this;
-    let result = await self.tokenContract.methods.battleSessionsTime(tokenId, rare).call();
-    return result;
-}
-
-///
-Battle.prototype.waitTimeSession = async function (tokenId, rare) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-        /// 重试3次
-        retry(() => {
-            return self.tokenContract.methods.waitTimeSession(tokenId, rare).call();
-        }, { max_tries: 5, interval: 3000 }).then(function (result) {
-            resolve(result);
-        }).catch(function (error) {
-            reject(error);
-        });
-    });
-}
-
-/// 对应NFT已经挑战过几次怪物
-Battle.prototype.battleTimes = async function (tokenId) {
-    let self = this;
-    let result = await self.tokenContract.methods.battleTimes(tokenId).call();
-    return result;
-}
-
-/// 对应稀有度能挑战多少次怪物
-Battle.prototype.getLimitRateRare = async function (rare) {
-    let self = this;
-    let result = await self.tokenContract.methods.getLimitRateRare(rare).call();
-    return result;
-}
-
-
-Battle.prototype.getTransactionReceipt = async function (transaction) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-        self.web3.eth.getTransactionReceipt(transaction, function (error, result) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-}
-
-
-Battle.prototype.decodeLog = function (data) {
-    let self = this;
-    return self.web3.eth.abi.decodeLog([ {
-        "name": "tokenId",
-        "type": "uint256"
-    }, {
-        "name": "monster",
-        "type": "uint8"
-    }, {
-        "name": "result",
-        "type": "uint8"
-    }, {
-        "name": "reward",
-        "type": "uint256"
-    }, {
-        "name": "exp",
-        "type": "uint256"
-    }], data ,['0x22020c10ce04134fcde4cba3315ea35a2653b8eb84fb95cb72b94c10f0d236b5','0x000000000000000000000000685905ed3dbe7f962ff4a3456fc4383d5cbda91e']);
-}
-
-module.exports = Battle;
+module.exports = AutoBattle;
